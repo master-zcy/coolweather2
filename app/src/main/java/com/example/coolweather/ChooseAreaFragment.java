69d4410be79b3d0a60a25da0e9305ea1d7b91733
zcy <840355496@qq.com>
2018-04-26 11:04:53 +0800

    新增切换城市和更新天气功能

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -91,13 +91,27 @@ public class ChooseAreaFragment extends Fragment {
                     selectCity = cityList.get(position);
                     queryCounties();
                 }else if(currentLevel==LEVEL_COUNTY){
+
                     String weatherId=countyList.get(position)
                             .getWeatherId();
-                    Intent intent=new Intent(getActivity(),
-                            WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+
+                    if(getActivity() instanceof MainActivity){
+
+                        Intent intent=new Intent(getActivity(),
+                                WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+
+                    }else if(getActivity() instanceof  WeatherActivity) {
+
+                        WeatherActivity activity= (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefreshLayout.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
+
                 }
             }
         });
